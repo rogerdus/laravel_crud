c004143c5727394797a3adc20ebd9dd582844402
rogelio interino <rogerdus@gmail.com>
2018-04-19 11:25:57 -0500

    Hello World Laravel

---
+++ app/Article.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    /**
+    *
+    *
+    *
+    *@var arra
+    */
+    protected $fillable = [
+      'title','body'
+    ];
+}
